fcc6594abf919f557cf783d5c7de1e6d893a1b02
Adaías Magdiel <adaiasmagdiell@gmail.com>
2026-02-02 19:44:34 -0300

    feat: Add API route for season

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+use AdaiasMagdiel\Erlenmeyer\App;
+use App\Controllers\ApiController;
+
+require_once __DIR__ . '/bootstrap.php';
+
+$app = new App();
+
+// --- Api ----------
+$app->get('/api/season', [ApiController::class, 'season']);
+
+$app->run();
