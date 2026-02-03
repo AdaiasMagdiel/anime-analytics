4732d30258638ebf512027b5ff9a2f17f5b37f3e
Adaías Magdiel <adaiasmagdiell@gmail.com>
2026-02-03 12:18:08 -0300

    feat: Add root and analytics endpoints
    - Add '/api/season/analytics' endpoint.
    - Add '/api/year/[year]/analytics' endpoint.

--- index.php
+++ index.php
@@ -7,10 +7,6 @@ require_once __DIR__ . '/bootstrap.php';
 
 $app = new App();
 
-$app->get('/', function ($req, $res, $params) {
-    return $res->withHtml(file_get_contents(__DIR__ . '/index.html'));
-});
-
 // --- Api ----------
 $app->get('/api/season', [ApiController::class, 'season']);
 $app->get('/api/year/[year]', [ApiController::class, 'year']);
