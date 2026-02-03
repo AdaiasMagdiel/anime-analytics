8f715ba51a96f718bcf58ae158edd2a64212aff6
Adaías Magdiel <adaiasmagdiell@gmail.com>
2026-02-03 12:18:23 -0300

    feat: Add dev_log helper function - Adds dev_log function to log data when in development environment

--- app/helpers.php
+++ app/helpers.php
@@ -9,3 +9,13 @@ if (!function_exists('isDev')) {
         return in_array($_SERVER['REMOTE_ADDR'], $whitelist) || $_SERVER['HTTP_HOST'] ?? '' === 'localhost:5013';
     }
 }
+
+if (!function_exists('dev_log')) {
+    function dev_log(mixed $data)
+    {
+        if (isDev()) {
+            $message = is_string($data) ? $data : json_encode($data, JSON_PRETTY_PRINT);
+            error_log($message);
+        }
+    }
+}
