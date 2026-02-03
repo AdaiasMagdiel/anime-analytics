833e0fd71c50721711b08d7b49c62a70ad1c3087
Adaías Magdiel <adaiasmagdiell@gmail.com>
2026-02-03 14:16:38 -0300

    feat: add app state initialization - initialize app state with analytics and filters (mode, year, season)

---
+++ frontend/src/com/adaiasmagdiel/app/state.cljs
@@ -0,0 +1,9 @@
+(ns com.adaiasmagdiel.app.state
+  (:require [reagent.core :as r]))
+
+(defonce app (r/atom {:analytics {}
+                      :filters {:mode "season"
+                                :year (.getFullYear (js/Date.))
+                                :season (nth
+                                         ["winter" "spring" "summer" "fall"]
+                                         (quot (.getMonth (js/Date.)) 3))}}))
