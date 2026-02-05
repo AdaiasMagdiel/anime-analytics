021e52f59f628a134311a2f7ec974045bff64ee5
Adaías Magdiel <adaiasmagdiell@gmail.com>
2026-02-05 19:59:07 -0300

    feat: Add charts component to app - Add charts component import - Include charts/root in view - Fix indentation for api/fetch-analytics

--- frontend/src/com/adaiasmagdiel/app.cljs
+++ frontend/src/com/adaiasmagdiel/app.cljs
@@ -2,19 +2,21 @@
   (:require [reagent.dom.client :as rdom]
             [com.adaiasmagdiel.app.components :as c]
             [com.adaiasmagdiel.app.components.page-header :as page-header]
+            [com.adaiasmagdiel.app.components.charts :as charts]
             [com.adaiasmagdiel.app.api :as api]))
 
 (defn main-component []
   [:<>
    [c/header]
    [:main {:class "max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8"}
-    [page-header/root]]])
+    [page-header/root]
+    [charts/root]]])
 
 (defonce root
   (rdom/create-root (.getElementById js/document "root")))
 
 (defn ^:export init! []
-		(api/fetch-analytics)
+  (api/fetch-analytics)
   (rdom/render root [main-component]))
 
 (defn ^:dev/after-load reload! []
